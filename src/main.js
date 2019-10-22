915ddad10d26db768483c22bb07be48cecb70330
1jkl3 <3521941319@qq.com>
2019-10-22 15:45:00 +0800

    更新文件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import './assets/iconfont/iconfont.css'
 import './assets/css/reset.css'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(Element)
 Vue.config.productionTip = false
 
 
