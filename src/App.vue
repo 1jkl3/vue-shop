552a2d5330bec7e0b5af671bc8cfc99c136aacd1
1jkl3 <3521941319@qq.com>
2019-10-22 16:58:20 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <div class="nav">
+	  <el-header>
 		<tar></tar>
-    </div>
-    <router-view/>
+	  </el-header>
+	  <el-main>
+		<router-view/>
+	  </el-main>
   </div>
 </template>
 <script>
@@ -16,16 +18,17 @@
 	}
 </script>
 <style>
+body{
+	margin: 0px;
+	padding: 0px;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
 .nav {
-  padding: 30px;
+  padding: 0px;
 }
 
 .nav a {
